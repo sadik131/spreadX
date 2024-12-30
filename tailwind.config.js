dea4ec5e501eaea70494a6f47bdc217b6d30ebe2
supto <salatussadik131@gmail.com>
2024-12-31 01:48:27 +0530

    complite all page

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,22 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      borderRadius: {
+        "sm":"5px",
+      },
+      boxShadow: {
+        custom: '0px 1px 4px 0 rgba(0, 0, 0, 0.25)',
+      },
       colors:{
         primary:{
           default:"#2C5E9D",
-          secondary:"#2C89FF"
+          secondary:"#2C89FF",
+          accent:"#5A769B"
         },
-        accent:"#939393",
+        accent:{
+          default:"#939393",
+          secondary:"#2D425D"
+        },
         color:"#4D4D4D"
       },
       fontFamily: {
