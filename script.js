04db763ae2b1ca9c6ab44f1083d8a1d7f9c83c8b
supto <salatussadik131@gmail.com>
2025-01-02 23:31:52 +0530

    complite all

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
 
+// date picker
+document.addEventListener("DOMContentLoaded", function () {
+  const input = document.getElementById("datePicker");
+  // Reference the image and input field
+  const calendarIcon = document.getElementById("calendarIcon");
+  const datePickerInput = flatpickr("#datePicker", {
+    dateFormat: "m-d-Y", // Set desired format
+
+    // Event triggered when a date is selected
+    onChange: function () {
+      calendarIcon.style.display = "none";
+      input.classList.remove("pb-5");
+     
+    },
+  });
+
+  // Add event listener to the entire div to open Flatpickr
+  const container = document.getElementById("dateContainer");
+  container.addEventListener("click", function () {
+    datePickerInput.open();
+  });
+});
+
+// custom checkbox
 document.addEventListener('DOMContentLoaded', () => {
   const selectAllCheckbox = document.getElementById('select-all');
   const rowCheckboxes = document.querySelectorAll('.row-checkbox');
@@ -27,21 +51,26 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 
+// navbar
 const toggleBtn = document.getElementById('check');
 const sidebar = document.getElementById('sidebar');
 const statusText = document.getElementById('status-text');
 const menuItems = document.getElementById('menu-items');
+const collapes = document.getElementById('collapes');
+const arrow = document.getElementById('arrow');
 const sm = document.getElementById('smLogo'); 
 const lg = document.getElementById('lgLogo');
 
 
 toggleBtn.addEventListener('change', () => {
   if (toggleBtn.checked) {
-    console.log(toggleBtn.checked)
     sidebar.classList.remove('w-[8%]');
     sidebar.classList.add('w-[30%]');
     statusText.classList.remove('hidden');
     lg.classList.remove('hidden');
+    menuItems.classList.add('mt-12');
+    collapes.classList.remove('hidden');
+    arrow.classList.remove('rotate-180');
     sm.classList.add('hidden');
     
     const menuSpans = menuItems.querySelectorAll('span');
@@ -49,6 +78,9 @@ toggleBtn.addEventListener('change', () => {
       span.classList.remove('hidden'); 
     });
   } else {
+    arrow.classList.add('rotate-180');
+    menuItems.classList.remove('mt-12');
+    collapes.classList.add('hidden');
     sm.classList.remove('hidden');
     lg.classList.add('hidden');
     sidebar.classList.remove('w-[30%]');
@@ -59,5 +91,10 @@ toggleBtn.addEventListener('change', () => {
       span.classList.add('hidden'); 
     });
   }
-  console.log(sm)
 });
+
+// dropdown
+function toggleDropdown() {
+  const dropdown = document.getElementById('dropdown');
+  dropdown.classList.toggle('hidden');
+}
